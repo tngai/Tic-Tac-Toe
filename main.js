e896b1ba91ba14c170c17f7cf6fbe78afffe8c45
Tommy <Ngai@Tommys-MacBook-Pro.local>
2014-10-15 09:16:48 -0700

    angular implementation, basic

--- main.js
+++ main.js
@@ -1,27 +1,17 @@
-alert("js is loaded");
+
 var tttApp = angular.module("tttApp", []);
+	
 	tttApp.controller('TttController', function($scope){
 	$scope.board = [
-		{Box1Status:"u"},{Box1Status:"b"},{Box1Status:"c"},
-		{Box1Status:"d"},{Box1Status:"e"},{Box1Status:"f"},
-		{Box1Status:"g"},{Box1Status:"h"},{Box1Status:"i"},
-		{Box1Status:"j"},{Box1Status:"k"},{Box1Status:"l"},
-	]
-	$scope.boxSelect = function(boxNum) {
-		for(i = 1; i <= 9; i++) {
-			alert(i);
-			return;
-			if( i %2 != 0) {
-				alert(i)
-				return 
-				$scope.board[Box1Status[boxNum]] = "x";
-				mark = "moveX";
-				alert(mark)
-				return 
-			}
-			
-		}
+		{status:"0",pos:0},{status:"0", pos:1},{status:"0",pos:2},
+		{status:"0",pos:3},{status:"0", pos:4},{status:"0",pos:5},
+		{status:"0",pos:6},{status:"0", pos:7},{status:"0",pos:8},
+		
+	];
+	
+	$scope.boxSelect = function(cell) {
+		if(cell.status === "0"){alert("space is open")}
 
-	}
+	};
 
 });
